d2f266d75d88860b18aaa69e49b4d6c8bb38a16a
Yicun Hou <yicunhou@bu.edu>
2020-02-17 15:04:18 -0500

    Add Mapbox API support

--- config/environment.js
+++ config/environment.js
@@ -47,5 +47,7 @@ module.exports = function(environment) {
     // here you can enable a production-specific feature
   }
 
+  ENV.MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiYWtpc2VhcnUiLCJhIjoiY2s2bnpxYW92MDUzbTNtbWlmOHVzZm1tYyJ9.snBLN48-piJ6qKx68tPACQ';
+
   return ENV;
 };
